c25aa142fd86ffdc1a413862609f124860f2db86
David Voelkel <der.voelkel.david@gmail.com>
2015-11-22 14:24:37 +0100

    added terraform sec-groups, key & instance, replaced upstart with systemd / 14.04 with 15.04, added ansible deployment

--- infrastructure/network.tf
+++ infrastructure/network.tf
@@ -5,8 +5,41 @@ provider "aws" {
     region = "eu-central-1"
 }
 
-resource "aws_instance" "example" {
-    ami = "ami-900517fc"
-    instance_type = "t2.micro"
+resource "aws_security_group" "webservers" {
+  name = "webservers"
+  description = "Allow SSH and 8080 Web inbound and all outbound traffic"
+
+  ingress {
+      from_port = 22
+      to_port = 22
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+      from_port = 8080
+      to_port = 8080
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
 }
+resource "aws_key_pair" "webserver_key" {
+  key_name = "webserver_key" 
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC1sX+PRSGYYkPCQTupFalbyKB2V5LYocm+obrtGvm1iE7RnWcIYHiyhYdoa3ofVbyT7Vehqtn1/FOyBQW9eFCX4O/EB2MZ5jR5mHa2NE2WAna3KLaRUokvAcfSScAXu81YOFe3YWfsA8VJGNZj5M4hnJlUjtjHW0dXb4LJwWsWR+zJy3mZpFfEG5LgnUkM8Bt0+OcR3s/POoagrW0S7zNQisVZeWsXMF1b/BTWreUmeur6J1MsKVxD/H4SS/zUNniiAqYSSIySHts6+SwySq04DGu9ljyEyWU/NSPJL5aDvsvaTNdBB+dZIyHnOGGOVkEuIFitl36nuSmLAIWk0ntV david@david-Latitude-E6420"
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-a4e7f5c8"
+    instance_type = "t2.micro"
+    key_name = "webserver_key"
+    security_groups = ["webservers"]
+}
+
 
